4250eb1a2206791d9762cf3aa9c0ff266f18eca9
AswithaReddy <aswithareddy24@gmail.com>
2020-12-04 00:11:08 +0530

    services done

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/9b/e0b9ff6b9635001b1115b0e9d4f131dc
@@ -0,0 +1,15 @@
+package com.cg.attendance.repositories;
+
+
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import com.cg.attendance.domain.Employee;
+
+@Repository
+public interface EmployeeRepository extends CrudRepository<Employee,Long> {
+	
+}
+
+
--- attendance/src/main/java/com/cg/attendance/domain/Employee.java
+++ attendance/src/main/java/com/cg/attendance/domain/Employee.java
@@ -3,46 +3,57 @@ package com.cg.attendance.domain;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 import javax.persistence.OneToMany;
+import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 /**
- * This Java class contains all the employee details
+ * This Bean contains all the details of an employee
+ * 
  * @author Suparna Arya & Aswitha
  *
  */
-@Entity(name="employees")
+@Entity(name = "employees")
 public class Employee {
-	//empId is primary key for employees entity
+	// empId is primary key for employees entity
 	@Id
-	@NotEmpty(message="Id cannot be empty")
+    //@NotBlank(message="Employee Id is required")
+	//@Column(unique=true,updateable=false)
+    //@Size(min=8,max=10,message="Employee Id size must be in between 8 and 10")
+	@GeneratedValue(strategy=GenerationType.AUTO)
 	private long empId;
-	@NotNull(message="Employee Name is required")
+	@NotNull(message = "Employee Name is required")
 	private String empName;
-	@NotNull(message="Phone Number is required")
+	@NotNull(message = "Phone Number is required")
 	private String phoneNumber;
-	@NotNull(message="Mail ID is required")
+	@NotNull(message = "Mail ID is required")
 	private String empEmail;
-	@NotNull(message="Employee Location is required")
+	@NotNull(message = "Employee Location is required")
 	private String empLocation;
-	@NotEmpty(message="Id cannot be empty")
+	//@NotBlank(message="Supervisior Id is required")
 	private long supervisiorId;
-	
-	//Employee details has one to many relationship with Attendance Detail entity using empId
-	@OneToMany(mappedBy = "employee")
-	private List<AttendanceDetails> attendance = new ArrayList<>();
-	
-	public Employee( ) {
-			super();
-		}
-	
 
-	public Employee( long empId,String empName,String phoneNumber,String empEmail,String empLocation,
-		 long supervisiorId) {
+	// Employee details has one to many relationship with Attendance Detail entity
+	// using empId
+	@OneToMany(mappedBy = "employee")
+	private List<AttendanceDetail> attendance = new ArrayList<>();
+
+	// Default constructor for employee bean
+	public Employee() {
+		super();
+	}
+
+	// Parameterized constructor for employee bean
+	public Employee(long empId, String empName, String phoneNumber, String empEmail, String empLocation,
+			long supervisiorId) {
 		super();
 		this.empId = empId;
 		this.empName = empName;
@@ -50,15 +61,15 @@ public class Employee {
 		this.empEmail = empEmail;
 		this.empLocation = empLocation;
 		this.supervisiorId = supervisiorId;
-		
+
 	}
 
-	//setters and getters for Employee to access outside this class
+	// setters and getters for Employee to access outside this class
 	public long getEmpId() {
 		return empId;
 	}
 
-	public void setEmpId(Long empId) {
+	public void setEmpId(long empId) {
 		this.empId = empId;
 	}
 
@@ -94,11 +105,11 @@ public class Employee {
 		this.empLocation = empLocation;
 	}
 
-	public List<AttendanceDetails> getAttendance() {
+	public List<AttendanceDetail> getAttendance() {
 		return attendance;
 	}
 
-	public void setAttendance(List<AttendanceDetails> attendance) {
+	public void setAttendance(List<AttendanceDetail> attendance) {
 		this.attendance = attendance;
 	}
 
